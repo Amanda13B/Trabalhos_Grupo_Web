abb235e405f9e3f138f06fe168da34fda37334a6
Guilherme Baroni Marchiori <guibaroni4466700@gmail.com>
2020-09-22 21:16:25 -0300

    comeco do script

---
+++ Jogo_da_Forca/script.js
@@ -0,0 +1,24 @@
+let palavra = ["d", "e", "s", "e", "r", "t", "o"]
+
+function ChuteDeLetra(letraEscolhida)
+{
+    //Checar se a letra escolhida aparece na paalvra secreta
+    for(i = 0; i <= palavra.length; i++)
+    {
+        //Se a letra escolhida for igual
+        if(letraEscolhida == palavra[i])
+        {
+
+        }
+    }
+}
+
+function Acerto()
+{
+
+}
+
+function Erro()
+{
+
+}
