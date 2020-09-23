218ad8b4feeb472ac1f7b6b729f9c4a0f1600581
Guilherme Baroni Marchiori <guibaroni4466700@gmail.com>
2020-09-22 21:32:55 -0300

    mais progresso no script

--- Jogo_da_Forca/script.js
+++ Jogo_da_Forca/script.js
@@ -1,24 +1,47 @@
 let palavra = ["d", "e", "s", "e", "r", "t", "o"]
 
+let numeroDeErros = 0;
+
 function ChuteDeLetra(letraEscolhida)
 {
+    let acertos = 0;
+
     //Checar se a letra escolhida aparece na paalvra secreta
     for(i = 0; i <= palavra.length; i++)
     {
-        //Se a letra escolhida for igual
+        //Se a letra escolhida for igual, da um acerto e coloca a letra
         if(letraEscolhida == palavra[i])
         {
+            acertos += 1;
 
+            Acerto(letraEscolhida);
+        }
+
+        //Ao fim da palavra, se não tiver nenhum acerto, realizar um Erro de penalidade
+        if(i == palavra.length)
+        {
+            if(acertos == 0)
+            {
+                let imagemForca = document.getElementById("nome do id da imagem da forca")
+
+                Erro();
+
+                imagemForca = "local/da/imagem" + numeroDeErros.toString() + ".png"
+            }
         }
     }
 }
 
-function Acerto()
+//Funcao que irá colocar a letra correta na caixa
+function Acerto(letra)
 {
+    let posicaoAtualDoAcerto = document.getElementById("id da posicao")
 
+    posicaoAtualDoAcerto.textContent = letra.toString();
 }
 
+//Funcao que irá adicionar um membro ao homem palito, checando se chegou no limite
 function Erro()
 {
-
+    numeroDeErros += 1;
 }
