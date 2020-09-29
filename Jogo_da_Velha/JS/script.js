2f9e616a0afcb8ed2558e831df4d0cc0b186e345
Guilherme Baroni Marchiori <guibaroni4466700@gmail.com>
2020-09-29 20:16:16 -0300

    comeco do codigo

---
+++ Jogo_da_Velha/JS/script.js
@@ -0,0 +1,29 @@
+let nomePrimeiroJogador;
+let nomeSegundoJogador;
+
+let jogadorUmPeca = true;
+
+function NovoJogo()
+{
+    var pessoa1 = prompt("Por favor, coloque o nome do jogador 1:", "Jogador 1")
+
+    if(pessoa1 == null || pessoa1 == "")
+    {
+        pessoa1 = "Jogador 1";
+    }
+    else
+    {
+
+    }
+
+    var pessoa2 = prompt("Por favor, coloque o nome do jogador 2:", "Jogador 2")
+
+    if(pessoa1 == null || pessoa1 == "")
+    {
+        pessoa2 = "Jogador 2";
+    }
+    else
+    {
+
+    }
+}
