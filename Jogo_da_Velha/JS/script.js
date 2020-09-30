b8cfe6d19b3358f5d08eddb09c433beb37013134
Guilherme Baroni Marchiori <guibaroni4466700@gmail.com>
2020-09-29 21:43:58 -0300

    script progresso

--- Jogo_da_Velha/JS/script.js
+++ Jogo_da_Velha/JS/script.js
@@ -2,9 +2,13 @@ let nomePrimeiroJogador;
 let nomeSegundoJogador;
 
 let JogadorAtual = 1;
-let textoJogadorAtual
+let textoJogadorAtual;
 
 let jogadorX = true;
+
+let podeJogar = false;
+
+let arrayCasas = [];
 
 function NovoJogo()
 {
@@ -12,7 +16,7 @@ function NovoJogo()
     LimparCaixas();
 
     //Propt Pegar nome do Jogador 1
-    nomePrimeiroJogador = prompt("Por favor, coloque o nome do jogador 1:", "Jogador 1")
+    nomePrimeiroJogador = prompt("Por favor, coloque o nome do jogador 1:", "Jogador 1");
 
     if(nomePrimeiroJogador == null || nomePrimeiroJogador == "")
     {
@@ -24,9 +28,9 @@ function NovoJogo()
     }
 
     //Propt Pegar nome do Jogador 2
-    var nomeSegundoJogador = prompt("Por favor, coloque o nome do jogador 2:", "Jogador 2")
+    var nomeSegundoJogador = prompt("Por favor, coloque o nome do jogador 2:", "Jogador 2");
 
-    if(nomeSegundoJogador == null || nomeSegundoJogador == "")
+    if(nomeSegundoJogador == null || nomeSegundoJogador == "" || nomeSegundoJogador == nomePrimeiroJogador)
     {
         nomeSegundoJogador = "Jogador 2";
     }
@@ -52,60 +56,121 @@ function NovoJogo()
 }
 
 function LimparCaixas()
-{
+{  
+    if(arrayCasas.length != 0)
+    {
+        //Passa por cada casa que foi selecionada anteriormente e deixa vazio
+        for(let i = 0; i < arrayCasas.length; i++)
+        {
+            let caixa = document.getElementById("casa" + arrayCasas[i]);
 
+            caixa.src = "Imagens/Vazio.png";
+
+            //Deixar eles jogarem
+            podeJogar = true;
+        }
+
+        //Tirar os numeros do Array
+        arrayCasas = [];
+    }
+    else
+    {
+        //Deixar eles jogarem
+        podeJogar = true;
+    }
 }
 
 function Jogada(numeroDaCasa)
 {
-    //Pega a casa que o jogador clicou
-    let caixaClicada = document.getElementById("casa" + numeroDaCasa);
-
-    //Dependendo do que o jogador pegou, vai colocar um X ou um O dependendo de quem é o turno
-    if(jogadorX)
+    if(podeJogar)
     {
-        //Bota imagem correspondente
-        //caixaClicada.src = "imagemX";
+        //Pega a casa que o jogador clicou
+        let caixaClicada = document.getElementById("casa" + numeroDaCasa);
 
-        //Troca de peça, e logo, de jogador
-        jogadorX = false;
+        let casaRepetida = false;
 
-        alert("X");
-
-        if(JogadorAtual == 1)
+        if(arrayCasas.length != 0)
         {
-            JogadorAtual == 2
+            for(let i = 0; i < arrayCasas.length; i++)
+            {
+                if(numeroDaCasa == arrayCasas[i])
+                {
+                    casaRepetida = true;
+                }
+            }
 
-            textoJogadorAtual.innerHTML = nomeSegundoJogador;
+            if(casaRepetida)
+            {
+
+            }
+            else
+            {
+                arrayCasas.push(numeroDaCasa);
+            }
         }
-        else if(JogadorAtual == 2)
+        else 
         {
-            JogadorAtual == 1
+            arrayCasas.push(numeroDaCasa);
+        }    
 
-            textoJogadorAtual.innerHTML = nomePrimeiroJogador;
+        alert(arrayCasas + " " + casaRepetida);
+
+        if(casaRepetida == false)
+        {
+            textoJogadorAtual = document.getElementById("nome-jogador-atual");
+
+            //Dependendo do que o jogador pegou, vai colocar um X ou um O dependendo de quem é o turno
+            if(jogadorX)
+            {
+                //Bota imagem correspondente
+                caixaClicada.src = "Imagens/X.png";
+
+                //Troca de peça, e logo, de jogador
+                jogadorX = false;
+
+                if(JogadorAtual == 1)
+                {
+                    JogadorAtual == 2;
+
+                    textoJogadorAtual.innerHTML = nomeSegundoJogador;
+                }
+                else if(JogadorAtual == 2)
+                {
+                    JogadorAtual == 1;
+
+                    textoJogadorAtual.innerHTML = nomePrimeiroJogador;
+                }
+            }
+            else if(jogadorX == false)
+            {
+                //Bota imagem correspondente
+                caixaClicada.src = "Imagens/Circulo.png";
+
+                //Troca de peça, e logo, de jogador
+                jogadorX = true;
+
+                if(JogadorAtual == 1)
+                {
+                    JogadorAtual == 2;
+
+                    textoJogadorAtual.innerHTML = nomeSegundoJogador;
+                }
+                else if(JogadorAtual == 2)
+                {
+                    JogadorAtual == 1;
+
+                    textoJogadorAtual.innerHTML = nomePrimeiroJogador;
+                }
+            }
+        }
+        else
+        {
+            alert("Essa casa já foi selecionada!");
         }
     }
-    else if(jogadorX == false)
+    else
     {
-        //Bota imagem correspondente
-        //caixaClicada.src = "imagemO";
 
-         //Troca de peça, e logo, de jogador
-        jogadorX = true;
-
-        alert("O");
-
-        if(JogadorAtual == 1)
-        {
-            JogadorAtual == 2
-
-            textoJogadorAtual.innerHTML = nomeSegundoJogador;
-        }
-        else if(JogadorAtual == 2)
-        {
-            JogadorAtual == 1
-
-            textoJogadorAtual.innerHTML = nomePrimeiroJogador;
-        }
     }
+
 }
